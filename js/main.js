5bbbe2544972dafc9638e207c609841f28c2ffa3
Use your GitHub username <Enter your email address>
2022-01-09 02:24:23 -0700

    updated

--- js/main.js
+++ js/main.js
@@ -1,35 +1,11 @@
-var document;
-var today = new Date();
-var y = today.getFullYear();
-document.getElementById('current-year').innerHTML = y;
-
-var update = document.lastModified;
-document.getElementById('update-time').innerHTML = update;
-
-WebFont.load({
-    google: {
-        families: [
-            'Alegreya Sans',
-            'Montserrat'
-        ]
-    }
-})
-
 // Code for Array List
-
-const links = [
+const linksList = [
     {
         label: "Week 1 Notes",
         url: "week1/index.html"
-    }
-];
-
-    let tableContents = "<ul>";
-    for (let i = 0; i < links.length; i++) {
-        tableContents += "<li>";
-        tableContents += "<a href=" + links[i].url + ">" +links[i].label;
-        tableContents += "</li>";
-    }
-    tableContents += "</ul>";
-
-document.getElementById("links").innerHTML = tableContents;
+    }//,
+    // {
+    //     label: "Week 2 Notes",
+    //     url: "week2/index.html"
+    // }
+]
